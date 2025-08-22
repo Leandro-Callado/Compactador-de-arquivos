e3af3e67ae140994f0f8091fb89d155e4a959afa
Leandro Callado <leandrocallado2@gmail.com>
2025-08-21 22:54:39 -0300

    Add funções de compactação

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import zipfile, tarfile, os
+
+def compactar_zip(arquivos, saida):
+    with zipfile.ZipFile(saida, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for arquivo in arquivos:
+            zipf.write(arquivo, os.path.basename(arquivo))
+
+def compactar_tar(arquivos, saida):
+    with tarfile.open(saida, "w") as tar:
+        for arquivo in arquivos:
+            tar.add(arquivo, arcname=os.path.basename(arquivo))
+
+def compactar_tar_gz(arquivos, saida):
+    with tarfile.open(saida, "w:gz") as tar:
+        for arquivo in arquivos:
+            tar.add(arquivo, arcname=os.path.basename(arquivo))
