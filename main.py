7932be760319f3b05bf576706b301185b126f601
Leandro Callado <leandrocalla2@gmail.com>
2025-08-28 16:42:44 -0300

    Revert "teste de funcionalidade"
    This reverts commit b8c903568d455b18fd16886e821c86d70a5a9d20.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import zipfile, tarfile, os
 def compactar_zip(arquivos, saida):
     with zipfile.ZipFile(saida, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for arquivo in arquivos:
-            zipf.write(arquivo, os.path.basename(arquivo), compress_type=zipfile.ZIP_DEFLATED)
+            zipf.write(arquivo, os.path.basename(arquivo))
 
 def compactar_tar(arquivos, saida):
     with tarfile.open(saida, "w") as tar:
@@ -21,9 +21,3 @@ class compactadorApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Compactador by Leandro & Lucas")
-
- 
-# Teste a compactação em ZIP
-print("Testando a compactação em ZIP...")
-compactar_zip("teste do python.txt", "meus_arquivos.zip")
-print("Arquivo meus_arquivos.zip criado com sucesso!")
