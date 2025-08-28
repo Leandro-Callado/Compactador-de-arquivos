b8c903568d455b18fd16886e821c86d70a5a9d20
LEANDRO CASTILHO FEITOSA CALLADO <leandro.callado@campus.unimontes.int>
2025-08-28 13:01:24 -0300

    teste de funcionalidade

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import zipfile, tarfile, os
 def compactar_zip(arquivos, saida):
     with zipfile.ZipFile(saida, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for arquivo in arquivos:
-            zipf.write(arquivo, os.path.basename(arquivo))
+            zipf.write(arquivo, os.path.basename(arquivo), compress_type=zipfile.ZIP_DEFLATED)
 
 def compactar_tar(arquivos, saida):
     with tarfile.open(saida, "w") as tar:
@@ -22,3 +22,8 @@ class compactadorApp:
         self.root = root
         self.root.title("Compactador by Leandro & Lucas")
 
+ 
+# Teste a compactação em ZIP
+print("Testando a compactação em ZIP...")
+compactar_zip("teste do python.txt", "meus_arquivos.zip")
+print("Arquivo meus_arquivos.zip criado com sucesso!")
