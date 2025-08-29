0456eb65f53c4e381051a1767b4d9e5dbc673f6c
Leandro Callado <leandrocallado2@gmail.com>
2025-08-29 17:32:23 -0300

    Add interface

--- main.py
+++ main.py
@@ -16,3 +16,41 @@ def compactar_tar_gz(arquivos, saida):
     with tarfile.open(saida, "w:gz") as tar:
         for arquivo in arquivos:
             tar.add(arquivo, arcname=os.path.basename(arquivo))
+
+window = tk.Tk()
+window.title("Compactador by Leandro & Lucas")
+window.geometry("400x300")
+
+label = tk.Label(
+    text="Escolha o formato da compactação: ",
+    fg="black",
+    width=40,
+    height=4
+)
+label.pack(pady=20)
+
+Zip = tk.Button(
+    text="Compactar em ZIP",
+    width=20,
+    height=3,
+    bg="white",
+    fg="black",
+)
+tar = tk.Button(
+    text="Compactar em TAR",
+    width=20,
+    height=3,
+    bg="white",
+    fg="black",
+)
+targz = tk.Button(
+    text="Compactar em TAR.GZ",
+    width=20,
+    height=3,
+    bg="white",
+    fg="black",
+)
+Zip.pack(pady=1)
+tar.pack(pady=1)
+targz.pack(pady=1)
+window.mainloop()
